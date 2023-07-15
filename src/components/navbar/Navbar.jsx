2ae53cd722e38f277df7944381be0367b2a62028
Kagheni <Kaghenimbale@gmail.com>
2023-07-15 14:57:51 +0200

    work on the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,29 @@
+import { AiOutlineMenu } from 'react-icons/ai';
+import { BsSearch } from 'react-icons/bs';
+import { BiUser } from 'react-icons/bi';
+import { FiShoppingCart } from 'react-icons/fi';
+import './navbar.css';
+
 const Navbar = () => {
-  return <div>This is the navigation bar</div>;
+  return (
+    <div className="navbar">
+      <div className="left-side">
+        <div className="menu">
+          <AiOutlineMenu className="icon" />
+        </div>
+        <div className="logo">
+          <h2>
+            MZB<span>SHOES.</span>
+          </h2>
+        </div>
+      </div>
+      <div className="right-side">
+        <BsSearch className="icon" />
+        <BiUser className="icon" />
+        <FiShoppingCart className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
